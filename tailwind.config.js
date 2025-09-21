0e5bf4a4b60c44f3991682a3742115f09728267b
nilesh <nileshkashani45@gmail.com>
2025-09-21 21:17:39 +0530

    4rd

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        robomono: ["Roboto Mono", "monospace"], // custom font
+      },
+    },
   },
   plugins: [],
 }
